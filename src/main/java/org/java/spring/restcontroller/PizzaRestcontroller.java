19688d0933c0ee1428f8f8217193ea5ce6e17d12
EugeniuPeruzzi <jaka1997@live.it>
2023-12-13 11:53:12 +0100

    Add optional parameter to getIndex method

--- src/main/java/org/java/spring/restcontroller/PizzaRestcontroller.java
+++ src/main/java/org/java/spring/restcontroller/PizzaRestcontroller.java
@@ -7,9 +7,11 @@ import org.java.spring.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +22,14 @@ public class PizzaRestcontroller {
 	private PizzaService pizzaService;
 	
 	@GetMapping
-	public ResponseEntity<List<Pizza>> getIndex(){
-		List<Pizza> books = pizzaService.findAll();
-		
-		return new ResponseEntity<>(books, HttpStatus.OK);
+	public ResponseEntity<List<Pizza>> getIndex(Model model,
+			@RequestParam(required = false) String p){
+		List<Pizza> pizzas = p == null  
+				? pizzaService.findAll()
+				: pizzaService.findByName(p);
+	
+		return new ResponseEntity<>(pizzas, HttpStatus.OK);
 	}
+	
+	
 }
