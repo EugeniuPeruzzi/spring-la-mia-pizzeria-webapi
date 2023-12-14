aba42f1c952319515ed16e73e0f2fa26f12bffcb
EugeniuPeruzzi <jaka1997@live.it>
2023-12-14 15:01:29 +0100

    Update index.html and App.vue

--- pizza-front-end/src/App.vue
+++ pizza-front-end/src/App.vue
@@ -1,30 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// Importazioni
+import Index from './components/Index.vue';
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+// PROPS
+const pizzas = ref(null);
+
+// FUNCTIONS
+// Axios
+const getPizzas = async () => {
+  const data = await axios.get("http://localhost:8080/api/pizza");
+  pizzas.value = data.data;
+};
+
+onMounted(getPizzas);
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Index :pizzas = "pizzas"/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ pizza-front-end/src/components/Index.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+// Props
+const props = defineProps({
+    pizzas: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <h1>Pizze</h1>
+    <ul>
+        <li v-for="pizza in pizzas" :key="pizza.id" @click="$emit('openPizza', pizza.id)">
+            <h2>{{ pizza.name }}</h2>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+
+ul {
+    list-style-type: none;
+}
+li {
+    cursor: pointer;
+}
+</style>
